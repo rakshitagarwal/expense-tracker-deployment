f024b051c62da3171883a7541471c80706bc8ddc
rakshitagarwal <rakshitag13@gmail.com>
2023-01-13 18:24:38 +0530

    frontend in backend

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,40 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-var cors = require('cors')
+const express = require("express");
+var cors = require("cors");
 
-const sequelize = require('./util/database');
-const User = require('./models/users');
-const Expense = require('./models/expenses');
-const Order = require('./models/orders');
-const Forgotpassword = require('./models/forgotpassword');
+const sequelize = require("./util/database");
+const User = require("./models/users");
+const Expense = require("./models/expenses");
+const Order = require("./models/orders");
+const Forgotpassword = require("./models/forgotpassword");
 
-
-
-
-const userRoutes = require('./routes/user')
-const purchaseRoutes = require('./routes/purchase')
-const resetPasswordRoutes = require('./routes/resetpassword')
+const userRoutes = require("./routes/user");
+const purchaseRoutes = require("./routes/purchase");
+const resetPasswordRoutes = require("./routes/resetpassword");
 
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const { Console } = require("console");
 
 // get config vars
 dotenv.config();
 
-
 app.use(cors());
 
 // app.use(bodyParser.urlencoded());  ////this is for handling forms
-app.use(express.json());  //this is for handling jsons
+app.use(express.json()); //this is for handling jsons
 
-app.use('/user', userRoutes)
+app.use("/user", userRoutes);
 
+app.use("/purchase", purchaseRoutes);
 
-app.use('/purchase', purchaseRoutes)
+app.use("/password", resetPasswordRoutes);
 
-app.use('/password', resetPasswordRoutes);
-
-
+app.use((req, res) => {
+  Console.log('urlll', req.url);
+  res.sendFile(path.join(__dirname, `public/${req.url}`));
+})
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
@@ -46,11 +45,11 @@ Order.belongsTo(User);
 User.hasMany(Forgotpassword);
 Forgotpassword.belongsTo(User);
 
-sequelize.sync()
-    .then(() => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    })
-
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
